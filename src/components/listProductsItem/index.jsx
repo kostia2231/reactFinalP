b596a7364d1ee2720202afb8d0ca849a19eb2992
Your Name <you@example.com>
2024-10-13 23:11:28 +0200

    ListProduct Button: added

--- src/components/listProductsItem/index.jsx
+++ src/components/listProductsItem/index.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { TypographyH4 } from "../ui/typo/typographyH4";
 import DiscountBadge from "../ui/discountBadge";
+import { Button } from "../ui/button";
 
 export default function ListProductsItem({ item }) {
   const discount = Math.round(
@@ -9,10 +10,13 @@ export default function ListProductsItem({ item }) {
   return (
     <div className="border rounded-xl">
       <Link to={`/all-products/${item.title}`}>
-        <div className="bg-secondary h-[350px] w-full rounded-xl relative">
+        <div className="bg-secondary h-[350px] w-full rounded-xl relative group">
           {item.discont_price ? (
             <DiscountBadge discount={discount} moreStyle="absolute m-4" />
           ) : null}
+          <div className="absolute bottom-0 hidden w-full p-4 group-hover:block">
+            <Button className="bottom-0 w-full mx-auto">Add to cart</Button>
+          </div>
         </div>
         <div className="flex flex-col items-center justify-center gap-4 p-8 mb-auto">
           {/* <img src={item.image} alt="Sales Image" /> */}
